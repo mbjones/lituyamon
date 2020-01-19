c193ae78761f83ff7d61981858b2fd2edf5e0364
Matt Jones <gitcode@magisa.org>
2020-01-18 21:24:06 -0500

    Save grafana config in a volume.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker run --name lituya-pg -e POSTGRES_PASSWORD=lituya -e POSTGRES_DB=lituya -e POSTGRES_USER=lituya -e PGDATA=/pgdata -v lituya_data:/pgdata -p 5432:5432 --network lituya_net -d postgres
-docker run -d --name=grafana -p 3000:3000 --expose 5432 --network lituya_net grafana/grafana
+docker run -d --name=grafana -v grafana_data:/var/lib/grafana -p 3000:3000 --expose 5432 --network lituya_net grafana/grafana
