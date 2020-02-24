f5f2c227ae853c13f38d7417f86447d512a51d8e
Matt Jones <gitcode@magisa.org>
2020-02-24 00:45:15 -0900

    iInitial configuration of setuptools.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lituya Monitor
+
+This is a python package for monitoring systems on Lituya.
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lituyamon", # Replace with your own username
+    version="0.2.0",
+    author="Matt Jones",
+    author_email="gitcode@magisa.org",
+    description="Lituya monitoring package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mbjones/lituyamon",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
